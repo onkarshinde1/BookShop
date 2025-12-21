5fda7f053ad0ae53b9ce35eac3d1d0926cbbd313
Onkar Shinde <onkarshinde499@gmail.com>
2025-12-21 13:10:46 +0530

    Backend Done

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -15,11 +15,31 @@ export const signup = async(req , res) =>{
             password: hashpassword
         })
         await createdUser.save();
-        res.status(201).json({message:"User created successfully.."})
+        res.status(201).json({message:"User created succawaessfully.."})
     } catch (error) {
         console.log("Error :" + error.message);
         res.status(500).json({
             message:"Internal Server Error.."
         })
     }
-} 
+};
+
+export const login = async(req , res) =>{
+    try {
+        const {email , password } = req.body;
+        const user = await User.findOne({email});
+        const isMatch =await bcryptjs.compare(password , user.password);
+        if(!isMatch || !user){
+            return res.status(400).json({message : "Invalid Username and passwords"})
+        }else{
+            res.status(200).json({message:"login succesfully" , user:{
+                _id : user._id,
+                fullname : user.fullname,
+                email : user.email
+            }})
+        }
+    } catch (error) {
+        console.log("Error" , error.message);
+        res.status(500).json({message : " Internal Server error"})
+    }
+}
